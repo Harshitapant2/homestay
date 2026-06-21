f994ac89813c237e0672aaf07586f582c95ab2d3
Harshitapant2 <pantharshita2006@gmail.com>
2026-06-21 13:31:22 +0530

    Add files via upload
    second commit

---
+++ footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "20px",
+        textAlign: "center",
+        borderTop: "1px solid gray",
+      }}
+    >
+      Footer Section
+    </footer>
+  );
+}
+
+export default Footer;
